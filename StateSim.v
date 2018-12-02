8fb9d5245d6312be208ee2cfe836e164adfc85f7
gancherj <jrg358@cornell.edu>
2018-12-02 09:13:45 -0500

    statesim direct refinement

--- StateSim.v
+++ StateSim.v
@@ -3,8 +3,15 @@
 From mathcomp Require Import ssreflect ssrfun ssrbool ssrint eqtype ssrnat seq choice fintype rat finfun.
 From mathcomp Require Import bigop ssralg div ssrnum ssrint finset ssrnum ssrnat finmap.
 
-Require Import PIOA Meas Posrat Aux FastEnum Refinement Action Lifting.
+Require Import PIOA Meas Posrat Aux FastEnum Refinement Action Lifting CompPIOA.
 
+Lemma is_chan_comp {G D D' : ctx} (P : PIOA G D) (E : PIOA G D') (he : env P E) x :
+  is_chan (P ||| E) (inr x) = (x \in outputs P) || (x \in outputs E).
+  rewrite /is_chan //=.
+  rewrite mem_cat .
+  elim (andP he) => _ ; rewrite /closed; move => /eqP //= ->.
+  rewrite in_nil orFb mem_cat //=.
+Qed.
 
 Section SimpleStateInj.
   
@@ -66,4 +73,79 @@ Section SimpleStateInj.
     rewrite !ret_bind //=.
     done.
 Qed.
+
+  Theorem ss2 R : SimpleStateInj R -> refines P1 P2 hcomp.
+    case => h1 h2 h3 h4.
+    move => D' E HE g Hg.
+    exists g; split.
+    rewrite /protocol_for.
+    admit.
+    have: (run (P1 ||| E) g <$> fun p => ((R p.1.1, p.1.2), p.2)) = run (P2 ||| E) g.
+
+    induction g using last_ind.
+    rewrite /run //= /initDist //= !measE //= h1 //=.
+    rewrite /protocol_for all_rcons in Hg. 
+    move: Hg.
+
+    case x => [h | v]; move => Hg.
+    
+    rewrite !run_rcons.
+    rewrite -IHg.
+    rewrite !mbindA //= !mbindA.
+    apply mbind_eqP => y Hy; rewrite !measE.
+    case h => [hl | hr]; rewrite //=.
+    rewrite !app_h_comp_l !measE //=.
+    rewrite -h3 !measE //=.
+    apply mbind_eqP => z Hz; rewrite !measE //=.
+    rewrite !app_h_comp_r; rewrite //= !measE //=.
+    munder (rewrite measE); done.
+    done.
+    done.
+    rewrite is_chan_comp in Hg; last by done.
+
+    move/andP: Hg => [Hg1 Hg2].
+    elim (orP Hg1) => Hv.
+    remember (v \in inputs E) as ve; destruct ve; symmetry in Heqve.
+    rewrite !run_rcons //= !measE.
+    rewrite -IHg; last by done.
+    rewrite !measE; apply mbind_eqP => y Hy; rewrite !measE.
+    rewrite !app_v_comp_l_int.
+    rewrite -h2.
+
+    elim (app_vP P1 v y.1.1 Hv).
+    move=> m mu Htr ->; rewrite //= !measE; munder (rewrite !measE; munder (rewrite !measE)); done. 
+    done.
+    move => H ->; rewrite !measE; done.
+    done.
+    admit.
+    admit.
+    done.
+    elim (andP HE); done.
+    done.
+    done.
+
+    rewrite !run_rcons //= !measE -IHg; last by done.
+    rewrite !measE; apply mbind_eqP => y Hy; rewrite !measE.
+    simpl.
+    rewrite !(app_v_comp_l_ext).
+    rewrite !measE //= -h2.
+    rewrite !measE; munder (rewrite measE).
+    done.
+    done.
+    admit.
+    admit.
+    by rewrite Heqve.
+    by elim (andP HE).
+    done.
+    by rewrite Heqve.
+    rewrite !run_rcons //= !measE -IHg; last by done.
+    rewrite !measE; apply mbind_eqP => y Hy; rewrite !measE.
+    admit.
+
+    move => <-.
+    rewrite !measE.
+    apply mbind_eqP => r Hr.
+    rewrite !measE //=.
+Admitted.
+
 End SimpleStateInj.
